236c1c67ebb4015a5b1bdecb23d201e529df1d0f
ratusalmaa <ratusalmaalkk@gmail.com>
2024-08-21 10:41:23 +0700

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,12 @@
-it('Login Phoenix', () => {
+it('Login Phoenix & Create Quotation', () => {
     //Login
     cy.visit('/') // Untuk mengakses web phoenix-dev tanpa harus mengetikkan ulang url webnya (baseUrl dimasukkan di file cypress.config.js)
     cy.get('#login').type('ratu.salma@mceasy.co.id') // Input email
     cy.get('#password').type('Queen1212*{enter}') // Input password dan langsung klik enter
     //cy.get('.btn').click(); -- cara lainnya apabila benar-benar ingin mengklik button Log in
-    cy.get('.o_home_menu').should('be.visible'); // Memastikan halaman home sudah ditampilkan sebelum lanjut ke action berikutnya
-})
-it('Create Quotation', () => {
+
     //Create Quotation
+    cy.get('.o_home_menu').should('be.visible'); // Memastikan halaman home sudah ditampilkan sebelum lanjut ke action berikutnya
     cy.get('#result_app_6').should('be.visible').click(); // Mengklik icon Sales
     cy.get('div.d-xl-inline-flex').find('button').should('be.visible').click(); // Mengklik Button New untuk Create Quotation
     cy.get('#title_0').type('Quotation Testing Automation Ratu'); // Menginputkan Nama Quotation
@@ -16,8 +15,4 @@ it('Create Quotation', () => {
     .should('be.visible') // Memastikan pilihan dropdown sudah terlihat/ditampilkan
     .first() // Memilih Customer teratas/pertama di list dropdown
     .click(); // Mengklik customer yang sudah dipilih
-
-    //cy.get('#partner_id_0').should('be.visible').type('Rosalia Indah Transport, PT{enter}').should('have.value', 'Rosalia Indah Transport, PT'); -- tidak jadi dipakai, tapi tujuannya untuk menginputkan nama customer -> enter -> memastikan filed telah terisi
-    cy.get('#responsible_partner_id_0').click();
-    cy.get('ul.o-autocomplete--dropdown-menu.ui-widget.show.dropdown-menu.ui-autocomplete').should('be.visible').first();
 })
